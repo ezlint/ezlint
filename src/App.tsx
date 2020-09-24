b6132cc44437a1bf70ed9a70b029adc6a03fd862
Valerio Battaglia <vabatta@gmail.com>
2020-09-24 15:59:19 +0200

    fix: import of home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
+import Home from './components/pages/Home';
 
 function App() {
   return (
